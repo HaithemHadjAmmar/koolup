a7e884e8697fad1d0e905d7af10d83f80f977234
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2024-03-05 14:10:39 +0100

    updates

--- lib/homeScreen/HomeScreen.dart
+++ lib/homeScreen/HomeScreen.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../Constantes.dart';
 import '../customButtomNavigationBar/CustomButtomNavigationBar.dart';
 
@@ -118,10 +119,62 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Padding(
         padding: EdgeInsets.only(top: 18.h),
         child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
+          child: Column(
             children: [
-              // Add your content widgets here
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.w),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            '20',
+                            style: GoogleFonts.sen(
+                              fontSize: 52.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 6.h),
+                        Text(
+                          'RUNNING ORDERS',
+                          style: GoogleFonts.sen(
+                            color: Colors.grey,
+                            fontSize: 14.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            '05',
+                            style: GoogleFonts.sen(
+                              fontSize: 52.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 6.h),
+                        Text(
+                          'ORDER REQUEST',
+                          style: GoogleFonts.sen(
+                            color: Colors.grey,
+                            fontSize: 14.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
