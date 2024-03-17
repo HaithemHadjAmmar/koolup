5ea6be5a45a9a9935d0f415d5c19e44095658e27
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2024-03-17 17:57:30 +0000

    updates

--- lib/Auth/ForgotPassword.dart
+++ lib/Auth/ForgotPassword.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+import '../Constantes.dart';
+import 'LoginScreen.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
@@ -8,8 +15,164 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  String _email = '';
+
+  /// Login Function
+  void handleForgot() {
+    setState(() {});
+
+    if (_email.isNotEmpty &&
+        _email.contains('@')
+       ) {
+
+      Get.to(
+            () => LoginScreen(),
+        transition: Transition.fadeIn,
+        duration: Duration(milliseconds: 300),
+      );
+    } else {
+      errorSnackBar(context, 'E-mail Required');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFF341748),
+            ),
+            child: Column(
+                children: [
+                  Expanded(
+                    flex: 7,
+                    child: FractionallySizedBox(
+                      heightFactor: 1.2,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 150.h,
+                            left: 0,
+                            right: 0,
+                            child: Column(children: [
+                              Text(
+                                'Forgot Password',
+                                style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 28.sp,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(height: 3.h),
+                              Text(
+                                'Please sign in to your existing account',
+                                style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w400),
+                              )
+                            ]
+                            ),
+                          ),
+                          Positioned(
+                            top: 80.h,
+                            left: 10.w,
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: SizedBox(
+                                child: Image.asset(
+                                  'assets/backButton.png',
+                                  width: 50.w,
+                                  height: 50.h,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Positioned(
+                            top: 20.h,
+                            child: SizedBox(
+                              child: Image.asset(
+                                'assets/img2.png',
+                                width: 350.w,
+                                height: 350.h,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 12,
+                    child: FractionallySizedBox(
+                      heightFactor: 1.2,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(60),
+                            topRight: Radius.circular(60),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 25.w),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                SizedBox(height: 80.h),
+                                TextField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                    suffixIcon: Icon(Icons.email),
+                                    hintText: 'Enter your E-mail',
+                                    labelText: 'E-mail',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      borderSide: BorderSide(color: koolColor),
+                                    ),
+                                  ),
+                                  onChanged: (value) {
+                                    _email = value;
+                                  },
+                                  cursorColor: koolColor,
+                                ),
+
+                                SizedBox(height: 30.h),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                                    backgroundColor: Color(0xFFFF7622),
+                                    fixedSize: Size(330.8, 60),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    handleForgot();
+                                  },
+                                  child: Text(
+                                    'Send Code',
+                                    style: GoogleFonts.inter(
+                                      color: Colors.white,
+                                      fontSize: 20.sp,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+            )
+        )
+    );
   }
 }
