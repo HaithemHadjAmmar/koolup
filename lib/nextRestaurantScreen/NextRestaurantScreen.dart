312dad386b1773694053ad6a381e194b7f65c28d
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2024-04-20 10:44:12 +0100

    updates

--- lib/nextRestaurantScreen/NextRestaurantScreen.dart
+++ lib/nextRestaurantScreen/NextRestaurantScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,10 +21,15 @@ class NextRestaurantScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-           Stack(
-              children: [
-                Hero(
-                  tag: 'restaurant_image_${image}',
+          Stack(
+            children: [
+              Hero(
+                tag: 'restaurant_image_${image}',
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(24.r),
+                    bottomRight: Radius.circular(24.r),
+                  ),
                   child: Image.asset(
                     width: double.infinity,
                     height: 321.h,
@@ -30,39 +37,55 @@ class NextRestaurantScreen extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                Positioned(
-                  top: 35.h,
-                  left: 10.w,
-                  child: IconButton(
-                    icon: const Icon(Icons.arrow_back_ios),
-                    color: Colors.white,
-                    iconSize: 30.w,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
+              ),
+              Positioned(
+                top: 35.h,
+                left: 20.w,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      iconSize: 30.w,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+
+                    SizedBox(width: 240.w),
+
+                    IconButton(
+                      icon: const Icon(Icons.more_horiz),
+                      color: Colors.white,
+                      iconSize: 30.w,
+                      onPressed: () {
+                        // Add onPressed function for menu icon
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
           Padding(
-            padding: EdgeInsets.only(top: 320.w),
+            padding: EdgeInsets.only(top: 320.w, left: 10.w),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: kToolbarHeight),
                 Text(
-                  title,
-                  style: GoogleFonts.poppins(
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.bold,
+                 'Baguette & Baguette',
+                  style: GoogleFonts.sen(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-                SizedBox(height: 8.h),
+                SizedBox(height: 5.h),
                 Text(
-                  subtitle,
-                  style: GoogleFonts.poppins(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.normal,
+                  'Sousse, Khzema',
+                  style: GoogleFonts.sen(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
                     color: Colors.grey,
                   ),
                 ),
@@ -74,4 +97,3 @@ class NextRestaurantScreen extends StatelessWidget {
     );
   }
 }
-
