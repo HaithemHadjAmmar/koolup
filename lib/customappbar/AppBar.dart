926db4132e20e52b3666dcdba5853a16808030e8
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2024-04-29 14:22:15 +0100

    updates

--- lib/customappbar/AppBar.dart
+++ lib/customappbar/AppBar.dart
@@ -14,6 +14,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      scrolledUnderElevation: 0,
       centerTitle: true,
       title: Image.asset(
         'assets/logo2.png',
