d489c0905ffc832e1fcdc953bde23e2213a8b1d3
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2024-05-03 10:45:10 +0100

    updates

--- lib/CardComponent/fooddetails/FoodDetailsScreen.dart
+++ lib/CardComponent/fooddetails/FoodDetailsScreen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -23,127 +24,145 @@ class FoodDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         scrolledUnderElevation: 0,
-         centerTitle: false,
+        centerTitle: false,
         title: Text(
-            'Details',
-          style: GoogleFonts.sen(fontSize: 17.sp, fontWeight: FontWeight.w600)),
+          'Details',
+          style: GoogleFonts.sen(fontSize: 17.sp, fontWeight: FontWeight.w600),
+        ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios_new_rounded, size: 25.w),
           onPressed: () => Get.back(),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-              Hero(
-                tag: 'food_image_${UniqueKey().toString()}',
-                child: Image.asset(
-                  image,
-                  width: double.infinity,
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                    width: 30.w,
-                    fit: BoxFit.fitHeight,
-                    'assets/elipse.png'
-                ),
-            SizedBox(width: 5.w),
-                Padding(
-                  padding: EdgeInsets.only(top: 3.h),
-                  child:Text(
-              restauName,
-              style: GoogleFonts.poppins(fontSize: 16.sp, fontWeight: FontWeight.w400),
-                 ),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 16.h),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-            Text(
-              foodName,
-              style: GoogleFonts.poppins(fontSize: 20.sp, fontWeight: FontWeight.w700),
-            ),
-                SizedBox(height: 3.h),
-                Text(
-                 '2 filets de poulet épicés, laitue, fromage cheddar, sauce chili, sauce mayonnaise, 1 moyenne frite, canette au choix',
-                  style: GoogleFonts.poppins(fontSize: 14.sp, fontWeight: FontWeight.w300),
-                ),
-               ],
-             ),
-
-            SizedBox(height: 16.h),
-
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10.h),
-               child: Text(
-                  'SIZE:',
-                  style: GoogleFonts.sen(
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w400,
-                   ),
-                  ),
-                 ),
-                SizedBox(width: 5.w),
-                Container(
-                  width: 40.w,
-                  height: 40.h,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE2703D),
-                    borderRadius: BorderRadius.circular(80.r),
-                  ),
-                  child: Center(
-                    child: Text(
-                      '10',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w400,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Hero(
+                      tag: 'food_image_${UniqueKey().toString()}',
+                      child: Image.asset(
+                        image,
+                        width: double.infinity,
+                        fit: BoxFit.fitHeight,
                       ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/elipse.png', // Asset path was missing
+                          width: 30.w,
+                          fit: BoxFit.fitHeight,
+                        ),
+                        SizedBox(width: 5.w),
+                        Padding(
+                          padding: EdgeInsets.only(top: 3.h),
+                          child: Text(
+                            restauName,
+                            style: GoogleFonts.poppins(
+                                fontSize: 16.sp, fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.h),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          foodName,
+                          style: GoogleFonts.poppins(
+                              fontSize: 20.sp, fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(height: 3.h),
+                        Text(
+                          '2 filets de poulet épicés, laitue, fromage cheddar, sauce chili, sauce mayonnaise, 1 moyenne frite, canette au choix',
+                          style: GoogleFonts.poppins(
+                              fontSize: 14.sp, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.h),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.h),
+                          child: Text(
+                            'SIZE:',
+                            style: GoogleFonts.sen(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5.w),
+                        Container(
+                          width: 40.w,
+                          height: 40.h,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFE2703D),
+                            borderRadius: BorderRadius.circular(80.r),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '10',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
+            ),
+          ),
+          // Added container for price and button
+          Container(
+            width: double.infinity,
+            height: 160.h,
+            decoration: BoxDecoration(
+              color: Color(0xFFF0F5FA),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24.r),
+                topRight: Radius.circular(24.r),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Price: ${price.toStringAsFixed(2)}DT',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 16.sp),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement button action (e.g., add to cart, order now)
+                  },
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
               ],
             ),
-
-
-
-            Row(
-              children: [
-                Text(
-                  'Price: ${price.toStringAsFixed(2)}DT',
-                  style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 16.h),
-
-            ElevatedButton(
-              onPressed: () {
-                 // Implement button action (e.g., add to cart, order now)
-              },
-              child: Text(
-                'Add to Cart',
-                style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
